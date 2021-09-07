1c71766d7e28e7fed2fe8146c41901bae7b16d11
Justin Kelly <justin@kelly.org.au>
2021-09-07 15:58:57 +1000

    load csv - slv

--- database.go
+++ database.go
@@ -7,7 +7,7 @@ func createBiblioDB(env *Env) {
   }
   statement_entity.Exec()
 
-  statement, err := env.db.Prepare("CREATE TABLE IF NOT EXISTS stg_slv_primo (header_identifier TEXT, data_latest TEXT, metadata_identifier TEXT, metadata_identifier_handle_id TEXT, metadata_identidier_cms_id TEXT, metadata_identifier_accession_id TEXT, metadata_identifier_file_id TEXT, url TEXT)")
+  statement, err := env.db.Prepare("CREATE TABLE IF NOT EXISTS stg_slv_primo (header_identifier TEXT, date_latest TEXT, metadata_identifier TEXT, metadata_identifier_handle_id TEXT, metadata_identifier_cms_id TEXT, metadata_identifier_accession_id TEXT, metadata_identifier_file_id TEXT, url TEXT)")
   if err != nil {
     panic(err)
   }
